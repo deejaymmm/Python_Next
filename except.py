2ee42905a30b0003cfb0bca21d61dfb2b5c6bcc3
deejaymmm <djmmm@ukr.net>
2021-02-15 14:31:25 +0200

    add except.py file

---
+++ except.py
@@ -0,0 +1,30 @@
+def foo():
+    assert (1 == 2)
+
+try:
+    foo()
+except ZeroDivisionError:
+    print("ZeroDivisionError")
+except ArithmeticError:
+    print("ArithmeticError")
+except AssertionError:
+    print("AssertionError")
+except Exception:
+    print("Exception")
+except BaseException:
+    print("BaseException")
+
+"""Вашей программе будет доступна функция foo, которая может бросать исключения.
+
+Вам необходимо написать код, который запускает эту функцию, затем ловит исключения 
+ArithmeticError, AssertionError, ZeroDivisionError и выводит имя пойманного исключения.
+
+Пример решения, которое вы должны отправить на проверку.
+
+try:
+    foo()
+except Exception:
+    print("Exception")
+except BaseException:
+    print("BaseException")
+Подсказка: https://docs.python.org/3/library/exceptions.html#exception-hierarchy"""
