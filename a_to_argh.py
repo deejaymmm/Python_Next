5af1c88fb2e24c315993cd5ac2b90241dc650625
deejaymmm <djmmm@ukr.net>
2021-02-25 16:13:26 +0200

    change a_to_argh.py file

--- a_to_argh.py
+++ a_to_argh.py
@@ -1,1 +1,24 @@
-import reimport syspattern = r"\baa*\b"for line in sys.stdin:    string = line.rstrip()    print(re.sub(pattern, "argh", string, count=1, flags=re.IGNORECASE))"""Вам дана последовательность строк.В каждой строке замените первое вхождение слова, состоящего только из латинских букв "a" (регистр не важен), на слово "argh".Примечание:Обратите внимание на параметр count у функции sub.Sample Input:There’ll be no more "Aaaaaaaaaaaaaaa"AaAaAaA AaAaAaASample Output:There’ll be no more "argh"argh AaAaAaA"""
+import re
+import sys
+pattern = r"\baa*\b"
+for line in sys.stdin:
+    string = line.rstrip()
+    print(re.sub(pattern, "argh", string, count=1, flags=re.IGNORECASE))
+
+"""
+Вам дана последовательность строк.
+В каждой строке замените первое вхождение слова, состоящего только из 
+латинских букв "a" (регистр не важен), на слово "argh".
+
+Примечание:
+Обратите внимание на параметр count у функции sub.
+Sample Input:
+
+There’ll be no more "Aaaaaaaaaaaaaaa"
+AaAaAaA AaAaAaA
+Sample Output:
+
+There’ll be no more "argh"
+argh AaAaAaA
+"""
+
